0e1f565fdb15fdefbd26772c32743e0cc33a9f0e
Frexto23 <93665820+Frexto23@users.noreply.github.com>
2024-03-03 11:57:30 +0400

    3
    3

--- Hatton/js/get_data.js
+++ Hatton/js/get_data.js
@@ -1,13 +1,27 @@
-var new_category = $('<div class="new-category" id="-1">' +
-					'<input type="text" class="container-input category-name" placeholder="категория">' +
-					'<input type="text" class="container-input category-value" placeholder="руб">' +
-				'</div>)');
+function add_new_category() {
 
+	var new_category = $('<div class="new-category" id="-1">' +
+						'<input type="text" class="container-input category-name" placeholder="категория">' +
+						'<input type="text" class="container-input category-value" placeholder="руб">' +
+					'</div>)');
+
+	$(new_category).clone().appendTo(".categories");
+	$(".categories").attr('size', $(".categories").attr('size') * 1 + 1 * 1);
+	$(".categories").children().last().attr('id', $(".categories").attr('size'));
+	$(".categories").children().last().attr('spend_id', addSpending());
+}
 
 $(document).ready(function(){
+
+	add_new_category();
+
 	$(".add-new").click(function(){
-		$(new_category).clone().appendTo(".categories");
-		$(".categories").attr('size', $(".categories").attr('size') * 1 + 1 * 1);
-		$(".categories").children().last().attr('id', $(".categories").attr('size'));
+		add_new_category();
 	});
+
+	$('.container-input').on('input', function() {
+    	var updated = $(this).parent;
+    	updSpengind(updated.attr('spend_id'), updated.find(".category-name").val(), updated.find(".category-value"));
+	});
+
 });

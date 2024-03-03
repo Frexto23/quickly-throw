027928afb71e68e49a80b6ae808302b953b52aff
alexok <alexok2006@gmail.com>
2024-03-03 12:31:33 +0400

    ok

--- Hatton/js/graphics.js
+++ Hatton/js/graphics.js
@@ -14,7 +14,7 @@ function delSpending(id) {
 
 function updSpengind(id, new_name, new_cost) {
   setOfSpendings[id] = {name:new_name, cost:new_cost};
-  console.log(new_name+':'+new_cost);
+  //console.log(new_name+':'+new_cost);
 }
 
 function drawSpendings(canvId) {

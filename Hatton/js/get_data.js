7d5ba6ea6e7da3b85c2c3946aff4aad10a56f289
Frexto23 <93665820+Frexto23@users.noreply.github.com>
2024-03-03 11:42:04 +0400

    2
    2

--- Hatton/js/get_data.js
+++ Hatton/js/get_data.js
@@ -1,4 +1,4 @@
-var new_category = $('<div class="new-category">' +
+var new_category = $('<div class="new-category" id="-1">' +
 					'<input type="text" class="container-input category-name" placeholder="категория">' +
 					'<input type="text" class="container-input category-value" placeholder="руб">' +
 				'</div>)');
@@ -7,5 +7,7 @@ var new_category = $('<div class="new-category">' +
 $(document).ready(function(){
 	$(".add-new").click(function(){
 		$(new_category).clone().appendTo(".categories");
+		$(".categories").attr('size', $(".categories").attr('size') * 1 + 1 * 1);
+		$(".categories").children().last().attr('id', $(".categories").attr('size'));
 	});
 });
